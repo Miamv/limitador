87398597cb00701f0b3350a79001c3d8616b5ec2
miavvmm <mv.manchaca@alumno.um.edu.ar>
2026-08-07 20:07:29 -0300

    feat(app): estructura basica tres capas para spring boot

---
+++ src/main/java/ar/edu/um/ingenieria/limitador/LimitadorApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.um.ingenieria.limitador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/um/ingenieria/limitador/LimitadorApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.um.ingenieria.limitador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
